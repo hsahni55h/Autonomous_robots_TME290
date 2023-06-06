5c64ef84b4bc5a54e4847d9052242e7d758d3dc3
PrajwalChalmers <shetye@chalmer.se>
2023-06-06 18:07:10 +0200

    temp commit

--- Assignment4/lawnmower/src/tme290-lawnmower.cpp
+++ Assignment4/lawnmower/src/tme290-lawnmower.cpp
@@ -23,12 +23,14 @@
 #include <fstream> 		// for ofstream
 #include <string>
 
+using namespace std;
+using namespace tme290::grass;
+
 #include "cluon-complete.hpp"
 #include "tme290-sim-grass-msg.hpp"
 
 #define WALL_LENGTH   (22)
 #define WALL_WIDTH    (17)
-
 
 bool record_position{true};
 bool home_position{false};
@@ -38,34 +40,34 @@ bool charge_battery{false};
 bool return_charging_dock{false};
 bool stop_execution{false};
 
-uint32_t storedX{0};
-uint32_t storedY{0};
+uint32_t position_x{0};
+uint32_t position_y{0};
 
-const double rainLimit{0.4};
-std::string hourlyMeanFileName{"hourlyData"};
-std::string batteryFileName{"batteryData"};
+const double rain_threshold{0.4};
+string filename_hourly_average{"hourly_average_data"};
+string filename_battery{"battery_data"};
 
-void initCSVFile(std::string& fileName) 
+void initCSVFile(string& fileName) 
 {
-  auto now = std::chrono::system_clock::now();
-  std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+  auto now = chrono::system_clock::now();
+  time_t currentTime = chrono::system_clock::to_time_t(now);
 
-  std::stringstream ss;
-  ss << std::put_time(std::gmtime(&currentTime), "%m-%d_%H-%M");
+  stringstream ss;
+  ss << put_time(gmtime(&currentTime), "%m-%d_%H-%M");
 
   fileName = "data/" + fileName + "_" + ss.str() + ".csv";
 	return;
 }
 
-void appendToCSV(const std::string& line, const std::string& fileName) 
+void appendToCSV(const string& line, const string& fileName) 
 {
-  std::ofstream file(fileName, std::ios_base::app);
+  ofstream file(fileName, ios_base::app);
 
   if (!file.is_open()) {
     file.open(fileName);
     file.close();
 
-    file.open(fileName, std::ios_base::app);
+    file.open(fileName, ios_base::app);
   }
 
   if (file) {
@@ -74,7 +76,7 @@ void appendToCSV(const std::string& line, const std::string& fileName)
   }
 }
 
-double getBatteryLimit(tme290::grass::Sensors& msg) 
+double getBatteryLimit(Sensors& msg) 
 {
   uint32_t x{msg.i()}; 
   uint32_t y{msg.j()};
@@ -94,21 +96,21 @@ double getBatteryLimit(tme290::grass::Sensors& msg)
   return distance*0.005;
 }
 
-void cutGrass(tme290::grass::Sensors& msg, tme290::grass::Control& control) 
+void cutGrass(Sensors& msg, Control& control) 
 {
   uint32_t x{msg.i()}; 
   uint32_t y{msg.j()};
 
   if (x == 39 && y == 39) {
-    storedX = 23;
-    storedY = 18;
+    position_x = 23;
+    position_y = 18;
     record_position = false;
     return_charging_dock = true;
     move_right = true;
     return;
   } else if (x == 39 && y == 18) {
-    storedX = 0;
-    storedY = 0;
+    position_x = 0;
+    position_y = 0;
     record_position = false;
     return_charging_dock = true;
     move_right = true;
@@ -134,12 +136,12 @@ void cutGrass(tme290::grass::Sensors& msg, tme290::grass::Control& control)
   }
 }
 
-void stayInCell(tme290::grass::Control& control) 
+void stayInCell(Control& control) 
 {
   control.command(0);
 }
 
-void chargeRobot(tme290::grass::Sensors& msg, tme290::grass::Control& control) 
+void chargeRobot(Sensors& msg, Control& control) 
 {
   uint32_t x{msg.i()}; 
   uint32_t y{msg.j()};
@@ -147,14 +149,14 @@ void chargeRobot(tme290::grass::Sensors& msg, tme290::grass::Control& control)
   charge_battery = x != 0 && y != 0;
 
   if (record_position) {
-    storedX = x;
-    storedY = y;
+    position_x = x;
+    position_y = y;
     record_position = false;
   }
   if (x == 0 && y == 0 && !charge_level) {
-      std::string line{std::to_string(msg.time()) + "," +
-                       std::to_string(msg.battery())};
-      appendToCSV(line, batteryFileName);
+      string line{to_string(msg.time()) + "," +
+                       to_string(msg.battery())};
+      appendToCSV(line, filename_battery);
   }
 
   if (x == 0 && y == 0 && msg.battery() < 1.0) {
@@ -188,20 +190,20 @@ void chargeRobot(tme290::grass::Sensors& msg, tme290::grass::Control& control)
   }
 }
 
-void returnToPosition(tme290::grass::Sensors& msg, tme290::grass::Control& control) 
+void returnToPosition(Sensors& msg, Control& control) 
 {
   uint32_t x{msg.i()};
   uint32_t y{msg.j()};
 
-  if (x == storedX && y == storedY) {
+  if (x == position_x && y == position_y) {
     home_position = false;
     return;
   }
   
-  if (storedY < WALL_WIDTH) {
-    if (x == storedX) {
+  if (position_y < WALL_WIDTH) {
+    if (x == position_x) {
       control.command(6);
-    } else if (y == storedY) {
+    } else if (y == position_y) {
       control.command(4);
     } else {
       control.command(5);
@@ -212,10 +214,10 @@ void returnToPosition(tme290::grass::Sensors& msg, tme290::grass::Control& contr
       } else if (x < WALL_LENGTH && y == 16) {
         control.command(4);
       }
-    else if (storedX >= WALL_LENGTH) {
-      if (x == storedX) {
+    else if (position_x >= WALL_LENGTH) {
+      if (x == position_x) {
         control.command(6);
-      } else if (y == storedY) {
+      } else if (y == position_y) {
         control.command(4);
       } else {
         control.command(5);
@@ -223,9 +225,9 @@ void returnToPosition(tme290::grass::Sensors& msg, tme290::grass::Control& contr
     } else {
       if (y < 18) {
         control.command(6);
-      } else if (y == storedY) {
+      } else if (y == position_y) {
         control.command(8);
-      } else if (x == storedX) {
+      } else if (x == position_x) {
         control.command(6);
       } else {
         control.command(7);
@@ -239,15 +241,15 @@ int32_t main(int32_t argc, char **argv)
   int32_t retCode{0};
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
   if (0 == commandlineArguments.count("cid")) {
-    std::cerr << argv[0] 
-      << " is a lawn mower control algorithm." << std::endl;
-    std::cerr << "Usage:   " << argv[0] << " --cid=<OpenDLV session>" 
-      << "[--verbose]" << std::endl;
-    std::cerr << "Example: " << argv[0] << " --cid=111 --verbose" << std::endl;
+    cerr << argv[0] 
+      << " is a lawn mower control algorithm." << endl;
+    cerr << "Usage:   " << argv[0] << " --cid=<OpenDLV session>" 
+      << "[--verbose]" << endl;
+    cerr << "Example: " << argv[0] << " --cid=111 --verbose" << endl;
     retCode = 1;
   } else {
     bool const verbose{commandlineArguments.count("verbose") != 0};
-    uint16_t const cid = std::stoi(commandlineArguments["cid"]);
+    uint16_t const cid = stoi(commandlineArguments["cid"]);
     
     cluon::OD4Session od4{cid};
 
@@ -255,20 +257,20 @@ int32_t main(int32_t argc, char **argv)
 		double batteryLimit{0};
 
 		// TODO: some CSV operations here...
-		initCSVFile(hourlyMeanFileName);
-		initCSVFile(batteryFileName);
+		initCSVFile(filename_hourly_average);
+		initCSVFile(filename_battery);
 
     auto onSensors{[&od4, &timeStep, &batteryLimit](cluon::data::Envelope &&envelope)
       {
-        auto msg = cluon::extractMessage<tme290::grass::Sensors>(
-            std::move(envelope));
+        auto msg = cluon::extractMessage<Sensors>(
+            move(envelope));
         timeStep++;		// TODO: replace some variable
 
-        tme290::grass::Control control;
+        Control control;
 
 				batteryLimit = getBatteryLimit(msg);
 
-        if (msg.battery() < batteryLimit || (msg.rain() >= rainLimit && !home_position && msg.i() + msg.j() != 0)) {
+        if (msg.battery() < batteryLimit || (msg.rain() >= rain_threshold && !home_position && msg.i() + msg.j() != 0)) {
           return_charging_dock = true;
         } 
         
@@ -284,8 +286,8 @@ int32_t main(int32_t argc, char **argv)
           }
         }
 
-        std::cout << "Rain reading " << msg.rain() << ", direction (" <<
-         msg.rainCloudDirX() << ", " << msg.rainCloudDirY() << ")" << std::endl; 
+        cout << "Rain reading " << msg.rain() << ", direction (" <<
+         msg.rainCloudDirX() << ", " << msg.rainCloudDirY() << ")" << endl; 
 
         od4.send(control);
 				// TODO: some stop message
@@ -296,39 +298,40 @@ int32_t main(int32_t argc, char **argv)
 
     auto onStatus{[&verbose](cluon::data::Envelope &&envelope)
       {
-        auto msg = cluon::extractMessage<tme290::grass::Status>(
-            std::move(envelope));
+        auto msg = cluon::extractMessage<Status>(
+            move(envelope));
         if (verbose) {
-          std::cout  << "Status at time " << msg.time() << ": " 
-            << msg.grassMean() << "/" << msg.grassMax() << std::endl;
+          cout  << "Status at time " << msg.time() << ": " 
+            << msg.grassMean() << "/" << msg.grassMax() << endl;
         }
 
 				// TODO: some CSV thing
 				if (msg.time() % 60 == 0) {
-          std::string line{std::to_string(msg.time() / 60) + "," +
-                           std::to_string(msg.grassMean())};
-          appendToCSV(line, hourlyMeanFileName);
+          string line{to_string(msg.time() / 60) + "," +
+                           to_string(msg.grassMean())};
+          appendToCSV(line, filename_hourly_average);
         }
 
       }};
 
-    od4.dataTrigger(tme290::grass::Sensors::ID(), onSensors);
-    od4.dataTrigger(tme290::grass::Status::ID(), onStatus);
+    od4.dataTrigger(Sensors::ID(), onSensors);
+    od4.dataTrigger(Status::ID(), onStatus);
 
     if (verbose) {
-      std::cout << "All systems ready, let's cut some grass!" << std::endl;
+      cout << "All systems ready, let's cut some grass!" << endl;
     }
 
-    tme290::grass::Control control;
+    Control control;
     control.command(0);
     od4.send(control);
 
 		// TODO: some minor stuff in while
     while (od4.isRunning() && !stop_execution) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      this_thread::sleep_for(chrono::milliseconds(1000));
     }
 
     retCode = 0;
   }
   return retCode;
 }
+
